7aee84869768eae80ead54f55200ebaf9c3dbdeb
Andrei <andrei.yakimyuk>
2024-02-12 23:11:05 +0800

    solved task 2 and task 3

--- Seminar_4_HW/Program.cs
+++ Seminar_4_HW/Program.cs
@@ -37,10 +37,10 @@
 // }
 
 
-// Задайте массив заполненный случайными трёхзначными числами.
+// Задача 3. Задайте массив заполненный случайными трёхзначными числами.
 // Напишите программу, которая покажет количество чётных чисел в массиве.
 
-// Console.WriteLine("Введите массива: ");
+// Console.WriteLine("Введите размер массива: ");
 // int size = Convert.ToInt32(Console.ReadLine());
 // int[] numbers = new int[size];
 // FillArrayRandomNumbers(numbers);
@@ -74,4 +74,87 @@
 
 // Рабочее решение
 
+// решение второй задачи из примера: 
+// Задача 2: Подсчет количества четных чисел в массиве
+// using System;
+// class Program
+// {
+//     static void Main()
+//     {
+//         int[] numbers = new int[10]; // Массив для хранения случайных чисел
+//         Random random = new Random(); // Генератор случайных чисел
+//         int evenCount = 0; // Счетчик четных чисел
+//                            // Заполнение массива случайными трехзначными числами
+//         for (int i = 0; i < numbers.Length; i++)
+//         {
+//             numbers[i] = random.Next(100, 1000); // Случайное число от 100 до 999
+//             Console.Write(numbers[i] + " "); // Вывод сгенерированного числа
+//                                              // Проверка на четность и увеличение счетчика
+//             if (numbers[i] % 2 == 0)
+//             {
+//                 evenCount++;
+//             }
+//         }
+//         // Вывод количества четных чисел в массиве
+//         Console.WriteLine($"\nКоличество четных чисел в массиве: {evenCount}");
+//     }
+// }
 
+
+// Задача 3: Напишите программу, которая перевернёт одномерный массив 
+// (первый элемент станет последним, второй – предпоследним и т.д.)
+
+using System;
+ 
+public class Example
+{
+    public static void Main()
+    {
+        int[] array = { 5, 6, 4, 5, 9, 10 };
+        int[] numbers = array;
+        Console.Write("Исходный массив: ");
+        foreach (int number in numbers)
+         {
+             Console.Write(number + " ");
+         }
+        Array.Reverse(array);
+ 
+        Console.Write("\nПеревернутый массив: ");
+        Console.WriteLine(String.Join(',', array));
+    }
+}
+// Решение рабочее
+
+// Решение 3 задачи из примера
+// Задача 3: Реверсирование одномерного массива
+
+// using System;
+// class Program
+// {
+//     static void Main()
+//     {
+//         int[] numbers = { 1, 3, 5, 6, 7, 8 }; // Исходный массив
+//         int temp;
+//         // Вывод исходного массива
+//         Console.Write("Исходный массив: ");
+//         foreach (int number in numbers)
+//         {
+//             Console.Write(number + " ");
+//         }
+//         // Реверсирование массива
+//         for (int i = 0; i < numbers.Length / 2; i++)
+//         {
+//             // Меняем местами элементы
+//             temp = numbers[i];
+//             numbers[i] = numbers[numbers.Length - 1 - i];
+//             numbers[numbers.Length - 1 - i] = temp;
+//         }
+//         // Вывод измененного массива
+//         Console.Write("\nПеревернутый массив: ");
+//         foreach (int number in numbers)
+//         {
+//             Console.Write(number + " ");
+//         }
+//     }
+// }
+// рабочее решение
