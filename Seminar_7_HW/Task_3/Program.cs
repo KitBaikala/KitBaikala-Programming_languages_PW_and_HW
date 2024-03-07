b8259a91bce03238dc2437f66d18687827e6e8a6
Andrei <andrei.yakimyuk>
2024-03-07 12:17:34 +0800

    task_3 решена

---
+++ Seminar_7_HW/Task_3/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 3: Задайте произвольный массив. Выведете его элементы, 
+// начиная с конца. Использовать рекурсию, не использовать циклы.
+
+
+Console.Clear();
+Console.Write("Ввидите кол-во элементов в массиве: ");
+int n = int.Parse(Console.ReadLine()!);
+int i = 0;
+int[] array = new int[n];
+FillingArray(array, i);
+Console.WriteLine("Исходный массив: ");
+Console.WriteLine(PrintStartArray(array, i));
+Console.WriteLine("Результат: ");
+Console.WriteLine(PrintEndArray(array, i));
+
+int[] FillingArray(int[] array, int i)
+{
+    if (i < array.Length)
+    {
+        array[i] = new Random().Next(1, 100);
+        FillingArray(array, i + 1);
+    }
+    return array;
+}
+
+string PrintStartArray(int[] array, int i)
+{
+    if (i == array.Length - 1)
+    {
+        return $"{array[array.Length - 1]} ";
+    }
+    return PrintStartArray(array, i + 1) + $"{array[i]} ";
+}
+
+string PrintEndArray(int[] array, int i)
+{
+    if (i == array.Length - 1)
+    {
+        return $"{array[array.Length - 1]} ";
+    }
+    return $"{array[i]} " + PrintEndArray(array, i + 1);
+}
