f750601c6b767c409a78613e5315e141c5d99f8f
Andrei <andrei.yakimyuk>
2024-03-07 12:11:33 +0800

    task_2 решена

---
+++ Seminar_7_HW/Task_2/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 2: Напишите программу вычисления функции 
+// Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+Console.Clear();
+Console.Write("Задайте значение m: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Задайте значение n: ");
+int n = int.Parse(Console.ReadLine()!);
+int k = Ackermann(m, n);
+PrintResult(k);
+
+int Ackermann(int m, int n)
+{
+    if (m < 0 || n < 0)
+    {
+        return (-1);
+    }
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    if (m > 0 && n == 0)
+    {
+        return Ackermann(m - 1, 1);
+    }
+    if (m > 0 && n > 0)
+    {
+        return Ackermann(m - 1, Ackermann(m, n - 1));
+    }
+    return (0);
+}
+void PrintResult(int k)
+{
+    if (k == -1)
+    {
+        Console.WriteLine("Вводите только положительные числа");
+    }
+    else
+        Console.WriteLine(k);
+}
--- Seminar_7_HW/Task_2/obj/Debug/net6.0/Task_2.AssemblyInfo.cs
+++ Seminar_7_HW/Task_2/obj/Debug/net6.0/Task_2.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Seminar_7_HW")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Task_2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Seminar_7_HW")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Seminar_7_HW")]
+[assembly: System.Reflection.AssemblyProductAttribute("Task_2")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Task_2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
