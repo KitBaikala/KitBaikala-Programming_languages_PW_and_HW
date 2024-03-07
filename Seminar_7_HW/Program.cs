0492dae2be34d2ec9b0edf85b1f6a0645a5e3f2f
Andrei <andrei.yakimyuk>
2024-03-07 12:01:09 +0800

    task 1 решение

--- Seminar_7_HW/Program.cs
+++ Seminar_7_HW/Program.cs
@@ -3,33 +3,49 @@
 
 
 //  Задача 2: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
-Console.Clear();
-Console.Write("Введите число M: ");
-int m = int.Parse(Console.ReadLine());
-Console.Write("Введите число N: ");
-int n = int.Parse(Console.ReadLine());
-AkkermanFunction(m, n);
+// Console.Clear();
+// Console.Write("Введите число M: ");
+// int m = int.Parse(Console.ReadLine());
+// Console.Write("Введите число N: ");
+// int n = int.Parse(Console.ReadLine());
+// AkkermanFunction(m, n);
 
-void AkkermanFunction(int m, int n)
-{
-    Console.Write(Akkerman(m, n));
-}
-int Akkerman(int m, int n)
-{
-    if (m == 0)
-    {
-        return n + 1;
-    }
-    else if (n == 0 && m > 0)
-    {
-        return Akkerman(m - 1, 1);
-    }
-    else
-    {
-        return (Akkerman(m - 1, Akkerman(m, n - 1)));
-    }
-}
+// void AkkermanFunction(int m, int n)
+// {
+//     Console.Write(Akkerman(m, n));
+// }
+// int Akkerman(int m, int n)
+// {
+//     if (m == 0)
+//     {
+//         return n + 1;
+//     }
+//     else if (n == 0 && m > 0)
+//     {
+//         return Akkerman(m - 1, 1);
+//     }
+//     else
+//     {
+//         return (Akkerman(m - 1, Akkerman(m, n - 1)));
+//     }
+// }
 
 // Задача 3: Задайте произвольный массив. Выведете его элементы, 
 // начиная с конца. Использовать рекурсию, не использовать циклы.
 
+string NumbersFor(int a, int b)
+{
+string result = String.Empty;
+for (int i = a; i >= b; i--)
+{
+result += $"{i} ";
+}
+return result;
+}
+string NumbersRec(int a, int b)
+{
+if (a <= b) return NumbersRec(a + 1, b) + $"{a} ";
+else return String.Empty;
+}
+Console.WriteLine(NumbersFor(-16, 10)); // 10 9 8 7 6 5 4 3 2 1
+Console.WriteLine(NumbersRec(-1, 10)); // 10 9 8 7 6 5 4 3 2 1
